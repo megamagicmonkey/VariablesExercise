20381764e9dd88a30d0c95d8c1a91d58736a0575
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-19 14:25:52 -0500

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tString = "Test String";
+            int tInt = 10;
+            char tChar = 'T';
+            bool tBool = true;
+            double tDouble = 10.01;
+            decimal tDecimal = 10.02m;
+
+            Console.WriteLine("A printing out of test variables as follows:");
+            Console.WriteLine($"String:{tString} Integer:{tInt} Character:{tChar} Boolean:{tBool} Double:{tDouble} Decimal:{tDecimal}");
+        }
+    }
+}
